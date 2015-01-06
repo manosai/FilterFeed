3e114a3a21fefacee71ba62d3e96f28955fd9103
Manosai <mano.eerabathini@gmail.com>
2015-01-07 04:43:22 +0530

    display words in dashboard now works

--- home.js
+++ home.js
@@ -1,10 +1,28 @@
 $(document).ready(function() {
 
+	// check if there are already words in local storage, then display them
+
+
 	$("#submit_button").click(function (e) {
-		console.log("success"); 
+		var word = $("#keywords").val(); 
+		console.log(word); 
+
+		// add the word to local storage
+		chrome.storage.sync.set({'words': word}); 
 
 		// display the added words in the popup 
-
+		var html = ""; 
+		var ul = $("#navlist"); 
+		// adding additional keywords 
+		if (ul.length) {
+			html += "<li>" + word + "</li>"; 
+			$("#keyword_list ul").append(html); 
+		}
+		// adding the first keyword
+		else {
+			html += "<ul id ='navlist'><li>" + word + "</li></ul>"; 
+			$("#keyword_list").append(html);
+		} 
 
 		// clear the textbox
 		$("#keywords").val(''); 

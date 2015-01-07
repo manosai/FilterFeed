b03234512ede40523dff26f40175cf0980e4d9e6
Manosai <mano.eerabathini@gmail.com>
2015-01-07 05:33:19 +0530

    stores only last entered keyword

--- home.js
+++ home.js
@@ -1,14 +1,42 @@
+
 $(document).ready(function() {
-
 	// check if there are already words in local storage, then display them
+	chrome.storage.local.get('words', function(data) {
+		// words exist
+		if (!$.isEmptyObject(data)) {
+			console.log(data); 
+			console.log(data.words[0]); 
+			var counter = 0;  
+			var html = ""; 
+			for (i = 0; i < data.words.length; i++) {
+				console.log(data.words[i]); 
+				// adding the first word
+				if (i == 0) {
+					html += "<ul id ='navlist'><li>" + data.words[i] + "</li></ul>";
+					$("#keyword_list").append(html);
+				}
+				// adding the rest
+				else {
+					html += "<li>" + data.words[i] + "</li>"; 
+					$("#keyword_list ul").append(html); 
+				}
+			}
 
+		}
+	}); 
 
 	$("#submit_button").click(function (e) {
 		var word = $("#keywords").val(); 
 		console.log(word); 
-
+		var word_list = []; 
+		chrome.storage.local.get('words', function(data) {
+			for (item in data) { 
+				word_list.push(item); 
+			}
+		}); 
+		word_list.push(word); 
 		// add the word to local storage
-		chrome.storage.sync.set({'words': word}); 
+		chrome.storage.local.set({'words': word_list}); 
 
 		// display the added words in the popup 
 		var html = ""; 

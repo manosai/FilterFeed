a484fd4b4539f45661383bcfe068e723dcc52758
Manosai <mano.eerabathini@gmail.com>
2015-01-08 14:44:19 +0530

    filtering works

--- filterfeed.js
+++ filterfeed.js
@@ -1,14 +1,12 @@
-var killed_stories = [];
+var filtered_stories = [];
 
-function buzzkill(){
+function filterfeed(){
 
-  console.log("you should only see this on FB"); 
-
-  // be a buzz kill in news feed & groups
-  stories = document.getElementsByClassName("_5uch");
+  // collect posts in news feed 
+  stories = document.getElementsByClassName("_4ikz");
   for(var i=0; i < stories.length; i++){
     var story = stories[i];
-    killLinks(story, "feed");
+    filter(story, "feed");
 
   }
 
@@ -20,45 +18,36 @@ function buzzkill(){
   }
 }
 
-function killLinks(item, pageType){
-  var links = item.getElementsByTagName("a");
-  for(var k=0; k < links.length; k++){
-    var link = links[k];
-    var href = link.href.toLowerCase();
-    
-    // decide which type of link it is
-    var linkType = null;
-    if (href.indexOf("facebook.com/buzzfeed") !== -1 ){
-      linkType = "page link";
-    }
-    else if (href.indexOf("bzfd.it") !== -1 ){
-      linkType = "shortened link";
-    }
-    else if (href.indexOf("buzzfeed.com") !== -1 ){
-      linkType = "regular link";
-    }
+function filter(item, pageType){
 
-    // kill the story that contains this link
-    if(linkType !== null){
-      killItem(item, linkType, pageType);
+  // statuses with no links
+  var status_content= item.getElementsByTagName("p");
+  for(var k=0; k < status_content.length; k++){
+    var current_status = status_content[k];
+    // var href = status.href.toLowerCase();
+
+    // search for a keyword match
+    if (current_status.innerText.indexOf("Foster Farms") !== -1) {
+      console.log("do we get here"); 
+      filterItem(item); 
     }
   }
 }
 
-function killItem(item, linkType, pageType){
+function filterItem(item){
 
   // set the story to be invisible
   item.style.opacity = "0.0";
   item.style.display = "None";
 
   // add this story to the list of killed stories
-  if (killed_stories.indexOf(item) == -1){
-    console.log("killed a " + linkType + " on your " + pageType);
-    killed_stories.push(item);
+  if (filtered_stories.indexOf(item) == -1){
+    console.log("Just filtered" + item); 
+    filtered_stories.push(item);
   }
 
 }
 
-// be gone, productivity destroyers!
-buzzkill();
-document.addEventListener("scroll", buzzkill);
+// begin function call
+filterfeed();
+document.addEventListener("scroll", filterfeed);
